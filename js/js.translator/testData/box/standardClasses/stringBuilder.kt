f87b0c8f4e9b5c8181811153b377980d10e45b67
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2016-12-28 21:04:35 +0300

    wip

--- js/js.translator/testData/box/standardClasses/stringBuilder.kt
+++ js/js.translator/testData/box/standardClasses/stringBuilder.kt
@@ -6,14 +6,19 @@ inline fun foo(): Char? {
 }
 
 fun box(): String {
-    var q = "O${foo()}"
-    if (q != "OK") return "fail122"
+    if ('O'.toString() + "K" != "OK") return "fail toString"
 
-    val s = StringBuilder()
-    s.append("a")
-    s.append("b").append("c")
-    s.append('d').append("e")
-
-    if (s.toString() != "abcde") return s.toString()
+//
+//    if (!' '.isWhitespace()) return "fail white"
+//
+//    var q = "O${foo()}"
+//    if (q != "OK") return "fail122"
+//
+//    val s = StringBuilder()
+//    s.append("a")
+//    s.append("b").append("c")
+//    s.append('d').append("e")
+//
+//    if (s.toString() != "abcde") return s.toString()
     return "OK"
 }
