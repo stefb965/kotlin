53f202b740ddf0c420ccfeedc05a6cfdaf18cb16
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2017-01-12 21:06:35 +0300

    Support "Change parameter type" for parameters with type-mismatched default value  #KT-15627 Fixed

--- idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixFactoryForTypeMismatchError.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixFactoryForTypeMismatchError.kt
@@ -29,6 +29,7 @@ import org.jetbrains.kotlin.idea.core.quickfix.QuickFixUtil
 import org.jetbrains.kotlin.idea.util.approximateWithResolvableType
 import org.jetbrains.kotlin.idea.util.getResolutionScope
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfTypeAndBranch
 import org.jetbrains.kotlin.resolve.DescriptorToSourceUtils
 import org.jetbrains.kotlin.resolve.bindingContextUtil.getTargetFunction
 import org.jetbrains.kotlin.resolve.calls.callUtil.getParameterForArgument
@@ -179,6 +180,12 @@ class QuickFixFactoryForTypeMismatchError : KotlinIntentionActionsFactory() {
         if (annotationEntry != null) {
             if (KotlinBuiltIns.isArray(expectedType) && expressionType.isSubtypeOf(expectedType.arguments[0].type) || KotlinBuiltIns.isPrimitiveArray(expectedType)) {
                 actions.add(AddArrayOfTypeFix(diagnosticElement, expectedType))
+            }
+        }
+
+        if (diagnosticElement is KtExpression) {
+            diagnosticElement.getParentOfTypeAndBranch<KtParameter> { defaultValue }?.let {
+                actions.add(ChangeParameterTypeFix(it, expressionType))
             }
         }
 
